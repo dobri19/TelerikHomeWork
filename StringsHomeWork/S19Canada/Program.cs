1b9d4ee6bfd472609d46727f1e20167d210fd8c5
Dobri V <dobromir_varbanov@abv.bg>
2017-11-26 23:50:51 +0200

    Canada

---
+++ StringsHomeWork/S19Canada/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace S19Canada
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTimeFormatInfo formatInfo = (new CultureInfo("ca-CA")).DateTimeFormat;
+            //DateTime date1 = new DateTime(1, 12, 1);
+            DateTime date2 = new DateTime(2000, 10, 5);
+            Console.WriteLine(date2.ToString("yyyy,MM,dd"));
+            Console.WriteLine(date2);
+        }
+    }
+}
